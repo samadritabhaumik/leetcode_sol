aa6bf195b7cff34f826344db9c5d93cd69b61c4e
Samadrita Bhaumik <129898666+samadritabhaumik@users.noreply.github.com>
2025-08-02 12:08:24 +0530

    Time: 32 ms (89.65%) | Memory: 89.9 MB (76.55%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.cpp
+++ 2689-rearranging-fruits/rearranging-fruits.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     long long minCost(vector<int>& basket1, vector<int>& basket2) {
         int m = INT_MAX;
-        unordered_map<int, int> frequency_map;
+        unordered_map<int, int> mp;
         for (int b1 : basket1) {
-            frequency_map[b1]++;
+            mp[b1]++;
             m = min(m, b1);
         }
         for (int b2 : basket2) {
-            frequency_map[b2]--;
+            mp[b2]--;
             m = min(m, b2);
         }
         vector<int> res;
-        for (auto [k, c] : frequency_map) {
-            if (c % 2 != 0) {
+        for (auto [k, c]:mp) {
+            if (c%2!=0) {
                 return -1;
             }
-            for (int i = 0; i < abs(c) / 2; ++i) {
+            for (int i=0;i<abs(c)/2; ++i) {
                 res.push_back(k);
             }
         }
         if (res.empty()) return 0;
-        int half = res.size() / 2;
+        int half = res.size()/2;
         nth_element(res.begin(), res.begin() + half, res.end());
-        long long cost = 0;
+        long long count=0;
         for (int i = 0; i < half; ++i) {
-            cost += min(2LL * m, static_cast<long long>(res[i]));
+            count+=min(2LL * m, static_cast<long long>(res[i]));
         }
-        return cost;
+        return count;
     }
 };
