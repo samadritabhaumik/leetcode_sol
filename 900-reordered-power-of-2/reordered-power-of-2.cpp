7c7e140f81e761e1ffdd729acec12a566107fca3
Samadrita Bhaumik <129898666+samadritabhaumik@users.noreply.github.com>
2025-08-10 11:04:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.53%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string countDigits(int n) {
+    string solve(int n) {
         string count(10, '0');  
         while (n > 0) {
             count[n % 10]++;    
@@ -9,10 +9,10 @@ public:
         return count;      
     }
     bool reorderedPowerOf2(int n) {
-        string target = countDigits(n); 
+        string target = solve(n); 
         for (int i=0;i<31;++i) {  
             int power = 1 << i;          
-            if (countDigits(power) == target) {
+            if (solve(power) == target) {
                 return true;            
             }}
         return false;        
